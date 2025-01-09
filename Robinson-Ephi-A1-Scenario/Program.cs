ece54b84ff590886f41938a8cb3f4971bec1a4cd
Ephi Robinson <000969115@mohawkcollege.ca>
2025-01-09 11:39:04 -0500

    Rock paper scissors but you lose.

---
+++ Robinson-Ephi-A1-Scenario/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+string input = Console.ReadLine();
+Console.Write(input + " ");
+Console.Write(input + " ");
+Console.WriteLine(input + " ");
+if (input == ("Cookie"))
+    Console.WriteLine("I want a cookie too");
+if (input == ("Hamburger"))
+    Console.WriteLine("No hamburger for you!");
+if (input == ("Rock"))
+    Console.WriteLine("Paper");
+if (input == ("Paper"))
+    Console.WriteLine("Scissors");
+if (input == ("Scissors"))
+    Console.WriteLine("Rock");
+if (input == ("rock"))
+    Console.WriteLine("I Eat Rocks =)");
+if (input == ("paper"))
+    Console.WriteLine("I Eat Paper =(");
+if (input == ("scissors"))
+    Console.WriteLine("I DO NOT Eat Scissors!!");
+
+//Make squid game rock paper scissors for 1st assignment
