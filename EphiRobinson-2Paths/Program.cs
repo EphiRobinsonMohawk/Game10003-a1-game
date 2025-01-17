7dc969d55c0be264a6580e64d338e7bd76a2a3d0
Ephi Robinson <000969115@mohawkcollege.ca>
2025-01-16 20:43:07 -0500

    added more push

---
+++ EphiRobinson-2Paths/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("You come to a fork in the road");
+Console.WriteLine("Left or Right, Left is town Right is mountain");
+string input = Console.ReadLine();
+if  (input.Contains("ight"))
+{
+    Console.WriteLine("You walk right towards the massive mountain");
+}
+else if (input.Contains("eft"))
+{
+    Console.WriteLine("You walk left into the wonderful town");
+}
---
+++ Implementation challenge/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("WHO ARE YOU?");
+string input = Console.ReadLine();
+string name = input;
+Console.WriteLine($"WELCOME {name}?");
+Console.WriteLine("HOW DEEP ARE THINE POCKETS?");
+string pockets = Console.ReadLine();
+int gold = int.Parse(pockets);
+Console.WriteLine($"HEY {name}, Care to buy a bronze sword for 15 gold???");
+if (gold >= 15)
+{
+    Console.WriteLine("As you pull out your gold pouch the vendor grabs it and runs leaving you with nothing but the sword");
+    gold = 0;
+    Console.WriteLine($"You have {gold} gold"); 
+}
+else
+{
+    Console.WriteLine("If you don't have gold GET OUT!");
+    Console.WriteLine($"You have {gold} gold, and a sword");
+}
